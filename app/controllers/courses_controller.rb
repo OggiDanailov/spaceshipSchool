63bbdbd57205781d2d54b8433e9a943aac068e12
OggiDanailov <ogidan@abv.bg>
2019-01-14 12:39:54 -0500

    crud on courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -24,15 +24,27 @@ end
 	end
 
 	def show
+		@course = Course.find(params[:id])
 	end
 
 	def edit
+		@course = Course.find(params[:id])
+		@department = Department.find(params[:department_id])
 	end
 
 	def update
+		@department = Department.find(params[:department_id])
+		@course = @department.courses.find(params[:id])
+			if @course.update(course_params)
+				redirect_to "/departments/#{params[:department_id]}/courses/#{params[:id]}"
+			end	
 	end
 
 	def destroy
+		@department = Department.find(params[:department_id])
+		@course = @department.courses.find(params[:id])
+		@course.destroy
+		redirect_to courses_path
 	end
 
 	private
