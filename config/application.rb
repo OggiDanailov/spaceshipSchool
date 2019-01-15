67dad840ea922fcc647bd24dfc493c04be32ac0d
OggiDanailov <ogidan@abv.bg>
2019-01-14 20:56:06 -0500

    some ckeditor manipulations

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module SpaceshipSchool
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w( ckeditor/* )
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
