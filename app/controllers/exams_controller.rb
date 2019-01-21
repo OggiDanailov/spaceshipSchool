061bd0e2320d6ad8142193fcd823090e0a6001aa
OggiDanailov <ogidan@abv.bg>
2019-01-20 23:47:43 -0500

    instructor grading done; some formatting left plus some extra tests tomorrow to make sure that everything is working smoothly and there are not errors

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,6 +1,39 @@
 class ExamsController < ApplicationController
 
-	
+def new
+	@exam = Exam.new
+	@cohort = Cohort.find(params[:cohort_id])
+	@exams = Exam.all
+	@grades = [2,3,4,5,6]
+end	
+
+def create
+	@cohort = Cohort.find(params[:cohort_id])
+	@exam = @cohort.exams.new(exam_params)
+	@exam.student_id = params[:student_id]
+	@exam.instructor_id = current_instructor.id
+	graded = Exam.find_by(cohort_id: @cohort.id) && Exam.find_by(student_id: params[:student_id])
+	if(!graded)
+		@exam.save
+		redirect_to :controller => 'exams', :action => 'new'
+	else 
+		flash[:notice] = "You have laready graded this student"
+		redirect_to :controller => 'exams', :action => 'new'
+	end
+end
+
+def edit
+	@cohort = Cohort.find(params[:cohort_id])
+	@exam = Exam.find(params[:id])
+end
+
+def update
+	@cohort = Cohort.find(params[:cohort_id])
+	@exam = Exam.find(params[:id])
+	if @exam.update(exam_params)
+		redirect_to :controller => 'exams', :action => 'new'
+	end
+end
 
 
 
