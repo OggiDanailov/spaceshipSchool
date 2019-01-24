51e9b8be27b85129ea88f5bfddb3b7bf9d48fd99
OggiDanailov <ogidan@abv.bg>
2019-01-24 18:19:26 -0500

    let changed to var becuase of heroku

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,10 +1,10 @@
 $( document ).on('turbolinks:load', function() {
 
 
-	let gradeElement = document.getElementsByClassName("grade-element")
-	let gradeButton = document.getElementsByClassName("grade-button")
+	var gradeElement = document.getElementsByClassName("grade-element")
+	var gradeButton = document.getElementsByClassName("grade-button")
 
-	for(let i = 0; i< gradeElement.length; i++){
+	for(var i = 0; i< gradeElement.length; i++){
 		if(!isNaN(parseInt(gradeElement[i].innerHTML))){
 			gradeButton[i].style.visibility = 'hidden'
 		}
