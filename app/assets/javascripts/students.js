fb6f1d96ca68a843a5f7401bea007e6fb3ec3b27
OggiDanailov <ogidan@abv.bg>
2019-01-24 00:03:46 -0500

    have to fix the issue of not hiding button when loading page show student for the frist time

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,12 +1,11 @@
-$(document).ready(function(){
+window.onload = function(){
 	let gradeElement = document.getElementsByClassName("grade-element")
 	let gradeButton = document.getElementsByClassName("grade-button")
 
 	for(let i = 0; i< gradeElement.length; i++){
-		var temp = []
-		temp.push(gradeElement[i].innerHTML)
-			if(typeof(temp[i]) !== 'undefined'){
-				gradeButton[i].style.visibility = 'hidden'
-			}
+		if(!isNaN(parseInt(gradeElement[i].innerHTML))){
+			gradeButton[i].style.visibility = 'hidden'
+		}
+
 	}
-})
+}
