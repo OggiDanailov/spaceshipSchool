7e7be727cbcaa7de0ff11d720384a2fe2a1e6e72
OggiDanailov <ogidan@abv.bg>
2019-01-24 19:50:18 -0500

    some changes done

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -3,10 +3,12 @@ module StudentsHelper
 
 	def student_grade(cohorts) 
 		cohorts.exams.each do |e|
-			if e.student_id == current_student.id					
-				return e.grade
-			end
+
+			return current_student.id
 		end
 	end
 	
 end
+			# if e.student_id == current_student.id					
+			# 	return e.student_id
+			# end
